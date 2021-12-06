9c3f6f87bdc6c535d11f6da00331926949c13e67
Ole Nascimento <olenascimento@gmail.com>
2021-12-06 10:40:07 +0100

    get single record getone.php working

--- test/getall.php
+++ test/getall.php
@@ -1,5 +1,5 @@
 <?php
-
+// connection 
 include_once '../test/connection.php';
 
 // prepare and bind 
@@ -10,16 +10,15 @@ $result = $conn->query($sql);
 $commentArr = array();
 $commentArr["body"] = array();
 
-
 if ($result->num_rows > 0) {
   // output data of each row
   While($row = $result->fetch_assoc()) {
     extract($row);
     $e = array(
-       "id:" => $id,
-       "username" => $username,
-       "content" => $content,
-       "created" => $created
+      "id:" => $id,
+      "username" => $username,
+      "content" => $content,
+      "created" => $created
       );
     array_push($commentArr["body"], $e);
   }
@@ -29,5 +28,4 @@ if ($result->num_rows > 0) {
   echo "0 results";
 }
 $conn->close()
-
 ?>
---
+++ test/getone.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once '../test/connection.php';
+
+$id = isset($_GET['id']);
+
+// prepare and bind 
+$sql = "SELECT * FROM Comment WHERE id=?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+$dataRow = $result->fetch_assoc();
+
+http_response_code(200);
+echo json_encode([$dataRow]);
+
+
+
+
+?>
