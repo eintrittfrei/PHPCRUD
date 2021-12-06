acc603b91029b9822995a68083da855f976475f0
Ole Nascimento <olenascimento@gmail.com>
2021-12-06 18:43:41 +0100

    delete comment working

---
+++ test/delete.php
@@ -0,0 +1,20 @@
+<?php 
+
+include_once '../test/connection.php';
+
+  if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM Comment WHERE id=$id";
+
+    // prepare and bind 
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    
+    if ($conn->query($sql) === TRUE) {
+    echo "Record deleted";
+  } else { "error deleting record: " . $conn->error;
+  }
+  $conn->close();
+  }
+
+?>
