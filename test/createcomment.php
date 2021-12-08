0670389e9040929a5cde890a1335afe2c0e707a9
Ole Nascimento <olenascimento@gmail.com>
2021-12-08 13:28:32 +0100

    create comment working with hard coded data but not insomnia test

--- test/createcomment.php
+++ test/createcomment.php
@@ -7,16 +7,16 @@
   if (!($stmt = $conn->prepare("INSERT INTO Comment (username, content, created) VALUES (?,?,?)"))) {
     echo "Prepare failed: (" . $conn->error . ") " .$conn->error; 
   }
-  
+  if (!$stmt->bind_param("sss", $username, $content, $created)) {
+    echo "binding parameters failed: (" . $stmt->error . ") " . $stmt->error;
+}
   $username = 'username';
   $content = 'content';
   $created = 'created';
 
   // bind and execute 
 
-  if (!$stmt->bind_param("sss", $username, $content, $created)) {
-      echo "binding parameters failed: (" . $stmt->error . ") " . $stmt->error;
-  }
+  
 
   if (!$stmt->execute()) {
     echo "Execute failed: (" .$stmt->error . ") " . $stmt->error;
